1e3eccf968f634105eb5fe78439f0b5bf3b812f7
zyu <zhuo.yu@stud.tu-darmstadt.de>
2025-07-10 12:44:31 +0200

    feat: init base class for deep learning language detectors and initial model files

---
+++ src/implementation/models/base_language_detector.py
@@ -0,0 +1,9 @@
+from src.core.abstractions.model import AbstractModel
+
+
+class BaseDLLanguageDetector(AbstractModel):
+    """
+    Base calss for deep learning language detectors.
+    """
+    def __init__(self, config):
+        self.text_encoder =
